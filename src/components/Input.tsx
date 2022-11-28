11bb4dd2ae96cdaf566751d045bf59d288fb060b
Louisa Goncharenko <louisa.goncharenko@shopify.com>
2022-11-28 09:56:48 -0500

    added use states

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,22 +1,45 @@
-import React from 'react';
+import React, { useState }from 'react';
 import './Input.css';
 
 const Input = () => {
+    const [gif, setGif] = useState<string>("");
+    const [qty, setQty] = useState<number>();
+    async function fetchGIF (e: any) {
+        e.preventDefault();
+    }
+
   return (
-    <form className='gif-search'>
+    <>
+    <form onSubmit={fetchGIF}  className='gif-search'>
     <h1>Gif Search</h1>
     <fieldset>
             <label>
                 Type in a search query to search for GIFs:
-                <input type="text" name="search_query" />
+                <input 
+                type="text" 
+                value = {gif}
+                name="search_query"
+                onChange={(e) => setGif(e.target.value)}
+                 />
             </label>
             <label>
                 How many GIFs do you want?
-                <input type="number" name="quantity" min="1" max="10" value="5" />
+                <input 
+                type="number" 
+                value = {qty}
+                name="quantity" 
+                min="1" 
+                max="10"
+                onChange={(e) => setQty(parseInt(e.target.value))} 
+                />
             </label>
         <button className = 'search-button' type="submit"> Search For GIFs </button>
     </fieldset>
 </form>
+<section className='gif-results'>
+    {/* insert gifs here */}
+</section>
+</>
   )
 }
 
